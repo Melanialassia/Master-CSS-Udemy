aaab162ace3d28220d0cc27695c8beb335721ec2
Melani Alassia <alassiamelani1@gmail.com>
2024-03-28 12:31:45 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import Style from "./App.module.css";
+import style from "./App.module.css";
 
 function App() {
-
-
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={style.container}>
+      <header className={style.header}>
+        <div className={style.logo}>
+          <img src="public/logo.png" alt="logo" />
+        </div>
+        <h1>Master en CSS</h1>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
