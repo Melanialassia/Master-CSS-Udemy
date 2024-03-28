e21f2e879454c01ac6c9a9022a302913a73e0502
Melani Alassia <alassiamelani1@gmail.com>
2024-03-28 12:53:43 -0300

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,60 @@ function App() {
         </div>
         <h1>Master en CSS</h1>
       </header>
+      <nav className={style.menu}>
+        <ul>
+          <li>
+            <a href="#">
+Inicio
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Contenido
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Requerimientos
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Lenguajes
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Frameworks
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Proyectos
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Instructor
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Reseñas
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
