f9a511e413bc09dfa39249bd9ee3f0c60b27eca8
Melani Alassia <alassiamelani1@gmail.com>
2024-03-28 21:11:42 -0300

    information

--- src/App.jsx
+++ src/App.jsx
@@ -67,60 +67,269 @@ function App() {
           <div className={style.widget}>
             <h3>Ingresar</h3>
             <form action="">
-              <label htmlFor="">Usuario: </label>
+              <label htmlFor="">Usuario</label>
               <input type="text" />
 
-              <label htmlFor="">Constraseña: </label>
+              <label htmlFor="">Constraseña</label>
               <input type="password" />
 
               <input type="submit" value="Ingresar" />
             </form>
-              <a href="#"> Alumnos</a>
-              <a href="#"> Profesores</a>
-              <a href="#"> Calificaciones</a>
+            <a href="#"> Alumnos</a>
+            <a href="#"> Profesores</a>
+            <a href="#"> Calificaciones</a>
           </div>
           <div className={style.widget}>
-            <h3> Aula vitual 1</h3>
+            <h3 className={style.blue}> Aula virtual 1</h3>
             <form action="">
-              <label htmlFor="">Nombre de asuario: </label>
+              <label htmlFor="">Nombre de asuario </label>
               <input type="text" />
 
-              <label htmlFor="">Constraseña: </label>
+              <label htmlFor="">Constraseña </label>
               <input type="password" />
 
-              <input type="submit" value="Ingresar" />
+              <input type="reset" value="Ingresar" />
             </form>
           </div>
           <div className={style.widget}>
-            <h3>Aula vitual 2</h3>
+            <h3 className={style.red}>Aula virtual 2</h3>
             <form action="">
-              <label htmlFor="">Nombre de asuario: </label>
+              <label htmlFor="">Nombre de asuario</label>
               <input type="text" />
 
-              <label htmlFor="">Constraseña: </label>
+              <label htmlFor="">Constraseña</label>
               <input type="password" />
 
-              <input type="submit" value="Ingresar" />
+              <input type="reset" value="Ingresar" />
             </form>
           </div>
           <div className={style.widget}>
             <h3>Enlaces de interes</h3>
             <ul>
               <li>
-              <a href="https://www.bing.com/search?pglt=41&q=udemy&cvid=c9592551c5f94542a0302dfe1ab2273f&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBBFGDzSAQc4MzBqMGoxqAIAsAIA&FORM=ANSPA1&PC=DCTS">Udemy</a>
+                <a href="https://www.udemy.com/">Udemy</a>
               </li>
               <li>
-              <a href="https://www.soyhenry.com/webfullstack?utm_source=google&utm_medium=cpc&utm_campaign=GADS_PMAX_ARG&utm_content=fs&gad_source=1&gclid=Cj0KCQjwqpSwBhClARIsADlZ_TkfvYB1kRuXF88EWqfUXawrcqvxMKBN34rflEhaJdZ0RMH_oD7UrU0aAiWeEALw_wcB"> Bootcamp Henry</a>
+                <a href="https://www.soyhenry.com"> Bootcamp Henry</a>
               </li>
               <li>
-              <a href="https://www.youtube.com/@midulive"> Canal midudev</a>
+                <a href="https://www.youtube.com/@midulive"> Canal midudev</a>
               </li>
             </ul>
-          
           </div>
         </aside>
+        <div className={style.sections}>
+          <section>
+            <span>
+              <img
+                className={style.left}
+                src="public/scotch.png"
+                alt="sticker"
+              />
+              <img
+                className={style.right}
+                src="public/scotch.png"
+                alt="sticker"
+              />
+            </span>
+            <h2 className={`${style.title} ${style.red}`}>
+              Noticia de Front-end
+            </h2>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+          </section>
+          <section>
+            <span>
+              <img
+                className={style.left}
+                src="public/scotch.png"
+                alt="sticker"
+              />
+              <img
+                className={style.right}
+                src="public/scotch.png"
+                alt="sticker"
+              />
+            </span>
+            <h2 className={`${style.title} ${style.blue}`}>
+              Noticia de Back-end
+            </h2>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+          </section>
+          <section>
+            <span>
+              <img
+                className={style.left}
+                src="public/scotch.png"
+                alt="sticker"
+              />
+              <img
+                className={style.right}
+                src="public/scotch.png"
+                alt="sticker"
+              />
+            </span>
+            <h2 className={`${style.title} ${style.orange}`}>
+              Noticia de instructor
+            </h2>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+            <article>
+              <span className={style.icon}>a</span>
+              <p>
+                <a href="#">Titulo de entrada</a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                finibus dui sapien, in rutrum sem tempor ac. Nunc tristique,
+                justo et malesuada euismod, dui lectus aliquet orci, hendrerit
+                bibendum diam neque at felis.
+              </p>
+              <span className={style.date}>
+                [28-03-2024. Melani Alassia WEB]
+              </span>
+            </article>
+          </section>
+        </div>
+        <footer></footer>
       </div>
-      </>
+    </>
   );
 }
 
